17a0627c814c4abc1aaf46162158c2aa90bea479
trapcodeio <trapcode.io@gmail.com>
2022-08-14 09:42:59 +0100

    save

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -19,8 +19,25 @@ function onNumberOfWordsSettingsKeyUp(e: KeyboardEvent) {
 }
 </script>
 <template>
-  <div class="info">
-    Use this tool only if you have read: <a href="#">How It Works!</a>
+  <div class="info flex justify-center space-x-1">
+    <span>Use this tool only if you have read:</span>
+    <a href="#" class="inline"
+      ><span class="inline-block">How It Works!</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-4 w-4 inline-block -mt-1 ml-0.5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+        />
+      </svg>
+    </a>
   </div>
 
   <div class="p-3 mt-5 mb-3">
